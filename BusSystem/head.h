7e4f44a9af627914ec18ed1c022067fb8e525a4d
Stitch <467738357@qq.com>
2016-01-31 18:49:42 +0800

    Create project.

---
+++ BusSystem/head.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASTABLE -1
+#define OVERFLOW -2
+
+typedef int status;
+typedef int ElemType;   /*数据元素类型定义*/
